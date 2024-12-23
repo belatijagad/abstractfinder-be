a11342a9a628aa27371e5c4b7add13f88dc56ccb
Belati <belatijagadbintangsyuhada@gmail.com>
2024-12-24 02:17:29 +0700

    Refactor of retriever

--- api/routes/search.py
+++ api/routes/search.py
@@ -1,22 +1,23 @@
+from typing import List
 from fastapi import APIRouter, HTTPException
 from services.retrieval_service import RetrievalService
+from schemas.retrieval import SearchResult
 
 router = APIRouter()
-rs_service = RetrievalService()
+retrieval_service = RetrievalService()
 
 @router.get('/index')
 async def index() -> str:
 	try:
-		await rs_service.index()
-		return "success indexing"
+		await retrieval_service._create_index()
+		return 'Successfully indexed'
 	except Exception as e:
 		raise HTTPException(status_code=500, detail=str(e))
 
-@router.get('/search')
+@router.get('/search', response_model=List[SearchResult], response_model_exclude_none=True)
 async def search(query: str, k: int = 30):
   try:
-    results = rs_service.retrieve(query, k)  # Removed await
-    print(results)
-    return results.to_dict(orient="records")
+    results = retrieval_service.retrieve(query, k)
+    return [{'docno': doc['docno'], 'text': doc['text'], 'score': score} for doc, score in results]
   except Exception as e:
     raise HTTPException(status_code=500, detail=str(e))
